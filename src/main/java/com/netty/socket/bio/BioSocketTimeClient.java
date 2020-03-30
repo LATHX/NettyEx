03f37cdcf32eeb8463f86626a9c821965ae23bce
ljl <ljl@stu.jluzh.edu.cn>
2020-03-30 23:06:55 +0800

    Initial commit

---
+++ src/main/java/com/netty/socket/bio/BioSocketTimeClient.java
@@ -0,0 +1,30 @@
+package com.netty.socket.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class BioSocketTimeClient {
+    public static void main(String[] args) {
+        int port = 8080;
+        try {
+            Socket socket = new Socket("localhost", port);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),false);
+            out.println("QUERY TIME ORDER");
+            out.flush();
+            String response = in.readLine();
+            System.out.println("Now is:" + response);
+            out.close();
+            in.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/netty/socket/bio/BioSocketTimeServerHandler.java
@@ -0,0 +1,37 @@
+package com.netty.socket.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class BioSocketTimeServerHandler implements Runnable{
+    private Socket socket;
+
+    public BioSocketTimeServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try{
+            in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            out = new PrintWriter(this.socket.getOutputStream(),true);
+            while (true){
+                String body = in.readLine();
+                if(body == null){
+                    break;
+                }
+                if("QUERY TIME ORDER".equalsIgnoreCase(body)){
+                    out.println(new java.util.Date(System.currentTimeMillis()).toString());
+                }else{
+                    out.println("BAD ORDER");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
