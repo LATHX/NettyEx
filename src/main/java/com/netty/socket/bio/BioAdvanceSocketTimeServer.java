5ea05a5323c3b08844753dfe477028f8a24b87d0
ljl <ljl@stu.jluzh.edu.cn>
2020-03-31 23:34:30 +0800

    伪异步I/O，使用线程池处理Socket

--- src/main/java/com/netty/socket/bio/BioAdvanceSocketTimeServer.java
+++ src/main/java/com/netty/socket/bio/BioAdvanceSocketTimeServer.java
@@ -5,9 +5,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * 每次Socket都新建线程
+ * 使用线程池来处理Socket
  */
-public class BioSocketTimeServer {
+public class BioAdvanceSocketTimeServer {
     public static void main(String[] args) throws IOException {
         int port = 8080;
         ServerSocket serverSocket = null;
@@ -15,9 +15,10 @@ public class BioSocketTimeServer {
             serverSocket = new ServerSocket(port);
             System.out.println("The Time Server is running on " + port);
             Socket socket = null;
+            TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
             while (true) {
                 socket = serverSocket.accept();
-                new Thread(new BioSocketTimeServerHandler(socket)).start();
+                singleExecutor.execute(new BioSocketTimeServerHandler(socket));
             }
         } finally {
             if (serverSocket != null) {
