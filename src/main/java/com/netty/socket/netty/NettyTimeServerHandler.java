0c67b707ee7281b27e337fa3944c6920957e5be3
ljl <ljl@stu.jluzh.edu.cn>
2020-04-05 14:41:54 +0800

    Netty 时间服务器

---
+++ src/main/java/com/netty/socket/netty/NettyTimeServerHandler.java
@@ -0,0 +1,35 @@
+package com.netty.socket.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+
+public class NettyTimeServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        String currentTime = "";
+        if ("QUERY TIME ORDER".equalsIgnoreCase(body)) {
+            currentTime = new java.util.Date(System.currentTimeMillis()).toString();
+        } else {
+            currentTime = "BAD ORDER";
+        }
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
